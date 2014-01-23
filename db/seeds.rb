eb6f2f1600f3516d6ce40a6023284155569c704c
Kimberly Narine <kimberlyonarine@gmail.com>
2014-01-23 00:18:20 -0500

    Edit html

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+admin = User.create({email: 'admin@site.com', name: 'Site Admin', password: '123456'})
+admin.update_attribute("admin", 1)
+user = User.create({email: 'user@site.com', name: 'Site User', password: '123456'})
+
+book1 = Book.create({title: "Harry Potter", author: "JK Rowling", description: "You're a wizard Harry!", image: ":)"})
+Book.create({title: "Twilight", author: "Stephanie Meyers", description: "Vampires and werewolves", image: ":("})
+Book.create({title: "The Catcher in the Rye", author: "Some Dude", description: "That annoying kid", image: ":|"})
+
+3.times do |t|
+  Question.create({book_id: book1.id, user_id: admin.id, name: "Question#{t}", description: "This is a description of question #{t}."})
+end
