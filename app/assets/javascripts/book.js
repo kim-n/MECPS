4f7545df85f553dacec0d907322fed7888032c5d
Kimberly Narine <kimberlyonarine@gmail.com>
2014-01-25 01:41:46 -0500

    adds cs to book show page and JS to dynamically display comments

---
+++ app/assets/javascripts/book.js
@@ -0,0 +1,29 @@
+$(function(){
+    $(".question .comment-new").hide();
+    $(".question .comments-list").hide();
+    
+    $("body").on("click", ".responses-botton", function(event) {
+        event.preventDefault();
+        $thisQuestion = $(this).parent().parent();
+        $(".comments-list").hide()
+        
+        $thisQuestion.children(".comments-list").toggle();
+    });
+    
+    
+    $("body").on("click", ".comment-botton", function(event) {
+        event.preventDefault();
+        
+        $thisQuestion = $(this).parent().parent();
+        $thisQuestion.children(".comment-new").toggle();
+    });
+    
+    
+    
+    $("body").on("click", ".comment-exit", function(event) {
+        event.preventDefault();
+        
+        $thisQuestion = $(this).parent().parent();
+        $thisQuestion.children(".comment-new").hide();
+    });
+})
