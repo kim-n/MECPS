bed53533392564a31d81934dd055e6f4be05e77c
Kimberly Narine <kimberlyonarine@gmail.com>
2014-01-25 21:32:09 -0500

    updates css/html

--- app/assets/javascripts/book.js
+++ app/assets/javascripts/book.js
@@ -1,13 +1,20 @@
 $(function(){
-    $(".comment-new").hide();
-    $(".comments-list").hide();
-    // 
+    //if url is that of a book show page
+    if (window.location.pathname.indexOf("books") != -1){
+        $(".comment-new").hide();
+        $(".comments-list").hide();
+    }
+    
     //if url is that of a question show page
     if (window.location.pathname.indexOf("questions") != -1){
         $(".comments-list").show();
+        $(".comment-new").hide();
         $(".question > .comment-new").show();
         $(".responses-botton").hide();
     }
+    
+
+    
     
     $("body").on("click", ".responses-botton", function(event) {
         event.preventDefault();
